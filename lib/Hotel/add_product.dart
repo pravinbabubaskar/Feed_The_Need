e0a901aeb4c3792bf93b32d35ac475c38a0069be
kannathasann <kannathasan555viru@gmail.com>
2021-03-23 22:40:19 +0530

    Add_product page
    Add_product page updated

--- lib/Hotel/add_product.dart
+++ lib/Hotel/add_product.dart
@@ -6,11 +6,150 @@ class Add_product extends StatefulWidget {
 }
 
 class _Add_productState extends State<Add_product> {
+  final _key = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _key,
       appBar: AppBar(
-        title: Text("Add_product page"),
+          iconTheme: IconThemeData(color: Colors.black),
+          centerTitle: true,
+          elevation: 0.0,
+          backgroundColor: Colors.white,
+          title: Text(
+            "Add Product",
+            style: TextStyle(color: Colors.black),
+          )),
+      body: ListView(
+        children: <Widget>[
+          SizedBox(
+            height: 40,
+          ),
+          GestureDetector(
+            onTap: null,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(color: Colors.black)),
+                  width: 140,
+                  height: 120,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: <Widget>[
+                        Icon(Icons.camera_outlined,
+                            size: 70, color: Colors.grey),
+                        Text("pick an image"),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 12, right: 12, bottom: 12),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.black, width: 0.2),
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        offset: Offset(2, 7),
+                        blurRadius: 7)
+                  ]),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 14),
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Product name",
+                      hintStyle: TextStyle(
+                          color: Colors.grey, fontFamily: "Sen", fontSize: 18)),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 12, right: 12, bottom: 12),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.black, width: 0.2),
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        offset: Offset(2, 7),
+                        blurRadius: 7)
+                  ]),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 14),
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Product description",
+                      hintStyle: TextStyle(
+                          color: Colors.grey, fontFamily: "Sen", fontSize: 18)),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 12, right: 12, bottom: 12),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.black, width: 0.2),
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        offset: Offset(2, 7),
+                        blurRadius: 7)
+                  ]),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 14),
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Price",
+                      hintStyle: TextStyle(
+                          color: Colors.grey, fontFamily: "Sen", fontSize: 18)),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Padding(
+              padding: const EdgeInsets.only(left: 12, right: 12, bottom: 12),
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 0.2),
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          offset: Offset(2, 7),
+                          blurRadius: 4)
+                    ]),
+                child: FlatButton(
+                  onPressed: () async {},
+                  child: Text("post"),
+                ),
+              )),
+        ],
       ),
     );
   }
