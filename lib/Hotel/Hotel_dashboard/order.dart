95eef8e4bf2533b763bdb0b144733ad8146331db
kannathasann <kannathasan555viru@gmail.com>
2021-03-27 21:45:37 +0530

    order
    order_page addeddd...

--- lib/Hotel/Hotel_dashboard/order.dart
+++ lib/Hotel/Hotel_dashboard/order.dart
@@ -1,3 +1,5 @@
+import 'package:feedthenead/helpers/style.dart';
+import 'package:feedthenead/widgets/custom_text.dart';
 import 'package:flutter/material.dart';
 
 class Order extends StatefulWidget {
@@ -10,8 +12,28 @@ class _OrderState extends State<Order> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Order_page"),
+        iconTheme: IconThemeData(color: black),
+        backgroundColor: white,
+        elevation: 0.0,
+        title: CustomText(text: "Orders"),
+        leading: IconButton(
+            icon: Icon(Icons.close),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
       ),
+      body: ListView.builder(
+          itemCount: 5,
+          itemBuilder: (_, index) {
+            return ListTile(
+              leading: CustomText(
+                text: "order",
+                weight: FontWeight.bold,
+              ),
+              title: Text("name"),
+              subtitle: Text("date"),
+            );
+          }),
     );
   }
 }
