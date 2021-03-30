76cdc37682fa9a554a13aded2b8524edf5ba41e1
Srikarthikeyan4006 <mksrikarthikeyansk4006@gmail.com>
2021-03-30 16:23:26 +0530

    hotel page

--- lib/near_me.dart
+++ lib/near_me.dart
@@ -106,7 +106,7 @@ class _NearMeState extends State<NearMe> {
                             Navigator.push(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) => HotelPage()),
+                                  builder: (context) => HotelPage(hotelData: hotel[index],)),
                             );
                           },
                           child: Container(
@@ -190,10 +190,5 @@ class _NearMeState extends State<NearMe> {
         ),
       ],
     );
-  }
-
-  void openHotelPage() {
-    Navigator.push(
-        context, MaterialPageRoute(builder: (context) => HotelPage()));
   }
 }
