02f7da6392de73e1d74d259df1f14af7470173df
kannathasann <kannathasan555viru@gmail.com>
2021-04-03 10:26:08 +0530

    explore
    search restaurent by name

--- lib/explore.dart
+++ lib/explore.dart
@@ -104,3 +104,117 @@ Widget buildResultCard(data) {
       ))));
 }
 */
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:feedthenead/data.dart';
+import 'package:feedthenead/hotel.dart';
+import 'package:flutter/material.dart';
+
+class Explore extends StatefulWidget {
+  @override
+  _ExploreState createState() => _ExploreState();
+}
+
+class _ExploreState extends State<Explore> {
+  String name = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        title: Card(
+          child: TextField(
+            decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search), hintText: 'Search by name...'),
+            onChanged: (val) {
+              setState(() {
+                name = "";
+                name = val;
+                if (name.isNotEmpty)
+                  print(
+                      name[0].toUpperCase() + name.substring(1).toLowerCase());
+              });
+            },
+          ),
+        ),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: (name != "" && name != null)
+            ? FirebaseFirestore.instance
+                .collection('hotel')
+                .where('name',
+                    isGreaterThanOrEqualTo:
+                        name[0].toUpperCase() + name.substring(1).toLowerCase())
+                .where('name',
+                    isLessThan: name[0].toUpperCase() +
+                        name.substring(1).toLowerCase() +
+                        'z')
+                .snapshots()
+            : FirebaseFirestore.instance.collection("hotel").snapshots(),
+        builder: (context, snapshot) {
+          return (snapshot.connectionState == ConnectionState.waiting)
+              ? Center(child: CircularProgressIndicator())
+              : ListView.builder(
+                  itemCount: snapshot.data.docs.length,
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot data = snapshot.data.docs[index];
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HotelPage(
+                                      hotelData: snapshot.data.docs[index],
+                                    )));
+                      },
+                      child: Card(
+                        child: Row(
+                          children: <Widget>[
+                            Image.network(
+                              data['imageUrl'],
+                              width: 150,
+                              height: 100,
+                              fit: BoxFit.fill,
+                            ),
+                            SizedBox(
+                              width: 25,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  data['name'],
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Text(
+                                  data['type'],
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w200,
+                                    fontSize: 15,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+        },
+      ),
+    );
+  }
+}
